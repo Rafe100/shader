88f62a8483f7fa53c798d2aa15329d416957b3dc
unknown <936523016@qq.com>
2020-09-23 22:33:07 +0800

    sss

---
+++ shaderProject/Assets/share/script/ShadowMap.cs
@@ -0,0 +1,91 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ShadowMap : MonoBehaviour
+{
+    public Shader shadowReplaceShader;
+    public RawImage DepthRawImage;
+    Camera shadowCamera;
+    public RenderTexture shadowMap;
+    int smWidth = 1024;
+    int smHeight = 1024;
+    string shadowMapPropertyName = "_sMap";
+    string shadowMapCameraPropertyName = "_cameraProp";
+    string shadowCameraWorldToLocalMatrixName = "_shadowCameraW2LMatrix";
+    string shadowCameraProjectionMatrixName = "_shadowCameraProjectMatrix";
+    string lightPosition = "_lightPos";
+    string cameraPositionPropertyName = "_camPos";
+    int cameraPositionId;
+    int lightPositionId;
+    int shadowMapPropertyId;
+    int shadowMapCameraPropertyId;
+    int shadowMapCameraW2LMatrixId;
+    int shadowMapCameraProjectMatrixId;
+    private void Awake()
+    {
+        Init();
+    }
+
+    void Init()
+    {
+        if (shadowCamera == null)
+        {
+            shadowCamera = GetComponent<Camera>();
+            if (shadowCamera == null)
+            {
+                Debug.LogError("need camera component");
+                return;
+            }
+        }
+        if (shadowMap == null)
+        {
+            shadowMap = new RenderTexture(smWidth, smHeight,16, RenderTextureFormat.Shadowmap);
+            shadowMap.name = "_sMap";
+        }
+      
+        this.DepthRawImage.texture = shadowMap;
+        //shadowCamera.SetReplacementShader(shadowReplaceShader, "");
+        cameraPositionId = Shader.PropertyToID(cameraPositionPropertyName);
+        shadowMapPropertyId = Shader.PropertyToID(shadowMapPropertyName);
+        shadowMapCameraPropertyId = Shader.PropertyToID(shadowMapCameraPropertyName);
+        shadowMapCameraW2LMatrixId = Shader.PropertyToID(shadowCameraWorldToLocalMatrixName);
+        shadowMapCameraProjectMatrixId = Shader.PropertyToID(shadowCameraProjectionMatrixName);
+        lightPositionId = Shader.PropertyToID(lightPosition);
+        shadowCamera.depthTextureMode = DepthTextureMode.Depth;
+        Shader.SetGlobalVector(cameraPositionId, new Vector4(Camera.main.transform.position.x, Camera.main.transform.position.y,
+            Camera.main.transform.position.z, 1.0f));
+        Shader.SetGlobalVector(lightPositionId, new Vector4(transform.position.x, transform.position.y, transform.position.z, 1.0f));
+        Shader.SetGlobalVector(shadowMapCameraPropertyId, new Vector4(shadowCamera.orthographicSize, shadowCamera.nearClipPlane,shadowCamera.farClipPlane));
+        Shader.SetGlobalTexture(shadowMapPropertyId, shadowMap);
+        Debug.Log("world2LocalMatrix " + shadowCamera.transform.worldToLocalMatrix);
+        Debug.Log("worldToCameraMatrix " + shadowCamera.worldToCameraMatrix);
+        Debug.Log("cameraProjectionMatrix : " + shadowCamera.projectionMatrix.ToString());
+        Debug.Log("after project matrix " + shadowCamera.projectionMatrix * shadowCamera.worldToCameraMatrix);
+        Shader.SetGlobalMatrix(shadowMapCameraW2LMatrixId, shadowCamera.worldToCameraMatrix );
+        Shader.SetGlobalMatrix(shadowMapCameraProjectMatrixId, shadowCamera.projectionMatrix);
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        UpdateShaderProperty();
+    }
+
+    void UpdateShaderProperty()
+    {
+        Shader.SetGlobalVector(cameraPositionId, new Vector4(Camera.main.transform.position.x, Camera.main.transform.position.y,
+           Camera.main.transform.position.z, 1.0f));
+        Shader.SetGlobalVector(lightPositionId, new Vector4(transform.position.x, transform.position.y, transform.position.z, 1.0f));
+        Shader.SetGlobalVector(shadowMapCameraPropertyId, new Vector4(shadowCamera.orthographicSize, shadowCamera.nearClipPlane, shadowCamera.farClipPlane));
+        Shader.SetGlobalMatrix(shadowMapCameraW2LMatrixId, shadowCamera.worldToCameraMatrix);
+        Shader.SetGlobalMatrix(shadowMapCameraProjectMatrixId, shadowCamera.projectionMatrix);
+    }
+}
---
+++ shaderProject/Assets/share/shader/sss.shader
@@ -0,0 +1,78 @@
+﻿Shader "Custom/sss"
+{
+    Properties
+    {
+        _MainTex ("Texture", 2D) = "white" {}
+		_Color("BaseColor",Color) = (1,1,1,1)
+	    _Pow("Pow", float) = 1
+    }
+    SubShader
+    {
+        // No culling or depth
+        Cull Off ZWrite on ZTest lequal
+
+        Pass
+        {
+            CGPROGRAM
+            #pragma vertex vert
+            #pragma fragment frag
+
+            #include "UnityCG.cginc"
+
+            struct appdata
+            {
+                float4 vertex : POSITION;
+                float2 uv : TEXCOORD0;
+				float4 normal : NORMAL;
+            };
+
+            struct v2f
+            {
+                float2 uv : TEXCOORD0;
+                float4 vertex : SV_POSITION;
+				float3 worldPos : TEXCOORD1;
+				float3 worldNormal : TEXCOORD2;
+            };
+
+            v2f vert (appdata v)
+            {
+                v2f o;
+                o.vertex = UnityObjectToClipPos(v.vertex);
+				o.worldPos = mul(unity_ObjectToWorld, v.vertex);
+				o.worldNormal = UnityObjectToWorldNormal(v.normal);
+                o.uv = v.uv;
+                return o;
+            }
+
+            sampler2D _MainTex;
+			sampler2D _sMap;
+			float4x4 _shadowCameraW2LMatrix;
+			float4x4 _shadowCameraProjectMatrix;
+			float4 _sMap_TexelSize;
+			float4 _lightPos;
+			float4 _camPos;
+			float _Pow;
+			half4 _Color;
+            fixed4 frag (v2f i) : SV_Target
+            {
+				float4 coord = mul(_shadowCameraProjectMatrix, mul(_shadowCameraW2LMatrix, float4(i.worldPos,1)));
+				coord.xyz = (coord.xyz + 1) / 2;
+				float shadowMapDepth = SAMPLE_DEPTH_TEXTURE(_sMap, coord.xy);
+				float exitDepth = coord.z;
+				float distance = shadowMapDepth - exitDepth;
+				float3 lightDir = normalize(_lightPos.xyz - i.worldPos);
+				//lightDir = normalize(lightDir + i.worldNormal);
+				float3 viewDir = normalize(_camPos.xyz - i.worldPos);
+				float lv = pow(saturate(dot(viewDir, -lightDir)), _Pow);
+
+                //fixed4 col = tex2D(_MainTex, i.uv);
+				fixed4 col = _Color;
+				col *= lv;
+				//col += float4(distance, distance, distance, 0);
+				col.a = 1.0;
+                return col;
+            }
+            ENDCG
+        }
+    }
+}
